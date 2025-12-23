c1bc40eeb02c9fc91136611f514e5c0473d71802
Rishav <rajrishav011@gmail.com>
2025-12-23 22:47:36 +0530

    feat: create Group model with schema for group management

---
+++ backend/models/Group.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const groupSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        members: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
